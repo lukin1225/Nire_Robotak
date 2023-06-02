36f6a8abd6ecef7b7a0d1aad59c7a4ae233180d3
lukin1225 <122275500+lukin1225@users.noreply.github.com>
2023-06-02 13:26:16 +0200

    Create gazebo_ikusi.launch.py

---
+++ src/inaki_acker/nire_robota/launch/gazebo_ikusi.launch.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription, ExecuteProcess, TimerAction
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PythonExpression
+
+from nav2_common.launch import RewrittenYaml
+
+
+def generate_launch_description():
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+    pkg_sim_car = get_package_share_directory('inaki_acker')
+    pkg_joy = get_package_share_directory('joy')
+    world = LaunchConfiguration("world")
+    headless = "False"
+
+    bringup_dir = get_package_share_directory('inaki_acker')
+    launch_dir = os.path.join(bringup_dir, 'launch')
+    world_config = DeclareLaunchArgument(
+          'world',
+          default_value=[os.path.join(pkg_sim_car, 'worlds', 'etxea.world.world'), ''],
+          description='SDF world file')
+
+    use_simulator = "True" # LaunchConfiguration('use_simulator')
+
+
+    # Specify the actions
+    start_gazebo_server_cmd = ExecuteProcess(
+        condition=IfCondition(use_simulator),
+        cmd=['gzserver', '-s', 'libgazebo_ros_init.so',  '-s', 'libgazebo_ros_factory.so', world],
+        cwd=[launch_dir], output='screen')
+
+    start_gazebo_client_cmd = ExecuteProcess(
+        condition=IfCondition(PythonExpression(
+            [use_simulator, ' and not ', headless])),
+        cmd=['gzclient'],
+        cwd=[launch_dir], output='screen')
+
+    rviz_config_path = os.path.join(pkg_sim_car, 'rviz', 'urdf_config.rviz')
+    start_rviz2_cmd = ExecuteProcess(
+        cmd=['rviz2','--display-config',rviz_config_path], 
+        cwd=[launch_dir], 
+        output='screen')
+
+    # # Gazebo launch
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, 'launch', 'gazebo.launch.py'),
+        )
+    )
+
+    car = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            # os.path.join(pkg_sim_car, 'launch', 'spawn_red_crash.launch.py'),
+            os.path.join(pkg_sim_car, 'launch', 'spawn_car.launch.py'),
+        )
+    )
+
+    ld = LaunchDescription()
+    ld.add_action(world_config)
+    ld.add_action(gazebo)
+    # ld.add_action(start_gazebo_server_cmd)
+    # ld.add_action(start_gazebo_client_cmd)
+    ld.add_action(TimerAction(
+            period=5.0,
+            actions=[start_rviz2_cmd]))
+    ld.add_action(car)
+    return ld
